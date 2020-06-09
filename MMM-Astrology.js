79fecfdeab4364273abd1edc09517f447d64fb5d
rafhtl <36855265+rafhtl@users.noreply.github.com>
2020-06-10 01:28:48 +0200

    Update MMM-Astrology.js

--- MMM-Astrology.js
+++ MMM-Astrology.js
@@ -1,7 +1,7 @@
 /* Magic Mirror
 * Module: MMM-Astrology
 *
-* By cowboysdude
+* By cowboysdude/ revisoned RDR
 *
 */
 
@@ -9,7 +9,7 @@ Module.register("MMM-Astrology", {
 
 	// Module config defaults.
 	defaults: {
-		updateInterval: 60*1000, // every 10 minutes
+		updateInterval: 10*60*1000, // every 1 minutes
 		animationSpeed: 10,
 		initialLoadDelay: 875, // 0 seconds delay
 		retryDelay: 1500,
@@ -19,6 +19,11 @@ Module.register("MMM-Astrology", {
 		maxWidth: "400px",
 		fadeSpeed: 7,
         tcolor: "white",
+        fontSize: "14px",
+        textalign: "left",
+        titlemarginleft: "40",
+        translation_languages: ["it"],
+        
 
         sign : {
 		"leo":"Leo",
@@ -58,8 +63,11 @@ Module.register("MMM-Astrology", {
         });
 		this.url = this.getUrl();
 		this.today = "";
+        this.sendSocketNotification('START_ASTROLOGY', this.config);
+        this.horoscope = "";
+        this.astroHoroscope = "";
         
-		this.scheduleUpdate();
+		
 	},
 
 	getDom: function() {
@@ -74,6 +82,7 @@ Module.register("MMM-Astrology", {
 		var displayIcon = this.config.displayIcon;
 		var wrapper = document.createElement("div");
 		wrapper.className = "wrapper";
+        
 		wrapper.style.maxWidth = this.config.maxWidth;
 
 		if (!this.loaded) {
@@ -84,20 +93,28 @@ Module.register("MMM-Astrology", {
 		}
             var starSign = this.config.sign[astro.sunsign];
 
-			var ssign =  document.createElement("div");
+			var ssign=  document.createElement("div");
+            ssign.className = "title_image";
 			ssign.style.color = this.config.scolor;
 			//ssign.setAttribute('style','margin-bottom: -50px');
-			ssign.innerHTML = "<img src = modules/MMM-Astrology/icons/1/"+starSign+".svg width=10% height=10%> " + starSign;
+			ssign.innerHTML = "<img src = modules/MMM-Astrology/icons/1/"+starSign+".svg width=10% height=10%> "+starSign; 
+
+            ssign.style.textAlign = this.config.textalign;
+            
+            
+
 			wrapper.appendChild(ssign);
 
 			var tcolor = this.config.tcolor;
 
 			var title = document.createElement("div");
 			title.classList.add("xsmall", "bright", "title");
+            
 			title.style.color = tcolor;
 			title.setAttribute('style','margin-top:0; padding-top:0; float: left');
+            title.style.float = this.config.textalign;
 			if (this.config.hScope == "daily"){
-			title.innerHTML =   "<br>Daily Horoscope for Today";//+ moment(astro.date).format("MM-DD-YYYY");
+			title.innerHTML =   "<br>Horoscope "+ moment(astro.date).format("DD-MM-YYYY");;//+ moment(astro.date).format("DD-MM-YYYY");
 			} else if (this.config.hScope == "week"){
 			title.innerHTML =   "<br>Horoscope for "+astro.week;
 			} else if (this.config.hScope == "month") {
@@ -108,8 +125,11 @@ Module.register("MMM-Astrology", {
 			wrapper.appendChild(title);
 
 		var des = document.createElement("p");
-		des.classList.add("small", "bright","desc");
-		des.innerHTML = "<br>"+astro.horoscope;
+        des.classList.add("small", "bright","desc");
+		des.id = "desHoro";
+        des.style.fontSize = this.config.fontSize;
+        des.style.textalign= this.config.textalign;
+		des.innerHTML = "<br>"+this.astroHoroscope;
 		wrapper.appendChild(des);
 
 		return wrapper;
@@ -122,13 +142,13 @@ Module.register("MMM-Astrology", {
 		var newSign = this.config.starSign;
 
 		if (hType == "daily") {
-			url = "http://horoscope-api.herokuapp.com/horoscope/today/"+ this.config.starSign;
+			url = "https://horoscope-api.herokuapp.com/horoscope/today/"+ this.config.starSign;
 		} else if (hType == "week") {
-			url = "http://horoscope-api.herokuapp.com/horoscope/week/"+ this.config.starSign;
+			url = "https://horoscope-api.herokuapp.com/horoscope/week/"+ this.config.starSign;
 		} else if(hType == "month" || hType == "yearly") {
-			url = "http://horoscope-api.herokuapp.com/horoscope/month/"+ this.config.starSign;
+			url = "https://horoscope-api.herokuapp.com/horoscope/month/"+ this.config.starSign;
 		}  else if(hType == "year" || hType == "yearly") {
-			url = "http://horoscope-api.herokuapp.com/horoscope/year/"+ this.config.starSign;
+			url = "https://horoscope-api.herokuapp.com/horoscope/year/"+ this.config.starSign;
 		}
 		else {
 			console.log("Error: Can't get horoscope URL" + response.statusCode);
@@ -137,10 +157,11 @@ Module.register("MMM-Astrology", {
 	},
 
 	processAstrology: function(data) {
-		this.today = data.Today;
-		this.astro = data;
-		console.log(this.astro);
-		this.loaded = true;
+        this.today = data.Today;
+        this.astro = data;
+        this.astroHoroscope = this.astro.horoscope;
+        console.log(this.astro);
+        this.loaded = true;
 	},
 
 	scheduleUpdate: function() {
@@ -152,15 +173,24 @@ Module.register("MMM-Astrology", {
 
 	getAstrology: function() {
 		this.sendSocketNotification('GET_ASTROLOGY', this.url);
+        
 	},
 
+        
+  
 	socketNotificationReceived: function(notification, payload) {
+        if (notification === "ASTRO_STARTED") {
+            this.scheduleUpdate();
+            this.updateDom(this.config.initialLoadDelay);
+        }    
 		if (notification === "ASTRO_RESULTS") {
 			this.processAstrology(payload);
 			this.updateDom(this.config.animationSpeed);
+            
+            return
+            
 		}
-		this.updateDom(this.config.initialLoadDelay);
-	},
+    },
     
     notificationReceived: function (noti, payload) {
         
@@ -179,6 +209,7 @@ Module.register("MMM-Astrology", {
             this.config.starSign = this.config.starSigns[this.sign_position];
             this.url = this.getUrl();
             this.getAstrology();
+            
             return;
           }
           if(payload.command=="previous"){
@@ -193,6 +224,7 @@ Module.register("MMM-Astrology", {
             this.config.starSign = this.config.starSigns[this.sign_position];
             this.url = this.getUrl();
             this.getAstrology();
+            
             return;
           }
         }
